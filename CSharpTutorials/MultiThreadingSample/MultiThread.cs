0bfcc02f2187aa5f7cffd36d3f5aa910fe92c45b
jinjupeng <im.jp@outlook.com>
2021-09-26 15:32:24 +0800

    多线程学习

---
+++ CSharpTutorials/MultiThreadingSample/MultiThread.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingSample
+{
+    class MultiThread
+    {
+        static void Main(string[] args)
+        {
+            // 创建新线程
+            Thread oneThread = new Thread(PrintNumber);
+            // 开启线程
+            oneThread.Start();
+
+            // 睡眠2s
+            // Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            // 线程等待，必须等待oneThread线程执行完毕后才能执行下一步操作
+            oneThread.Join();
+
+            // 主线程中输出
+            PrintNumber();
+            Console.ReadKey();
+        }
+
+        static void PrintNumber()
+        {
+            Console.WriteLine("开始......");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CSharpTutorials/MultiThreadingSample/ThreadPool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadingSample
+{
+    /// <summary>
+    /// 线程池
+    /// </summary>
+    class ThreadPool
+    {
+        //声明委托
+        private delegate string RunOnThreadPool(out int threadId);
+
+        private static void Callback(IAsyncResult ar)
+        {
+            Console.WriteLine("触发回调");
+            Console.WriteLine("异步状态：" + ar.AsyncState);
+            Console.WriteLine("是否是线程池的线程：" + Thread.CurrentThread.IsThreadPoolThread);
+            Console.WriteLine("ThreadId：" + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static string Test(out int threadId)
+        {
+            Console.WriteLine("Test开始");
+            Console.WriteLine("是否是线程池的线程：" + Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            threadId = Thread.CurrentThread.ManagedThreadId;
+            return "ThreadId：" + threadId;
+        }
+
+        static void Main(string[] args)
+        {
+            int threadId = 0;
+            RunOnThreadPool poolDelegate = Test;
+            var t = new Thread(() => Test(out threadId));
+            t.Start();
+            t.Join();
+
+            Console.WriteLine("Thread ID=" + threadId);
+
+            #region NetFramework
+
+            //https://stackoverflow.com/questions/45183294/begininvoke-not-supported-on-net-core-platformnotsupported-exception
+            //.net core 平台不支持委托开启线程，会抛异常：Operation is not supported on this platform.
+
+            //IAsyncResult ar = poolDelegate.BeginInvoke(out threadId, Callback, "测试是否可以回调");
+            //ar.AsyncWaitHandle.WaitOne();
+            //string result = poolDelegate.EndInvoke(out threadId, ar);
+            //Console.WriteLine("结果：" + result);
+
+            #endregion
+
+            #region NetCore
+
+            // Task默认使用线程池中的线程
+            Task task = Task.Run(() => poolDelegate(out threadId));
+            // 回调
+            task.ContinueWith((task) => Callback(task)); // Callback方法会在task的异步操作完成后被调用
+            task.Wait();
+
+            #endregion
+
+            Console.WriteLine("ID：" + threadId);
+
+            Console.ReadKey();
+        }
+    }
+}
