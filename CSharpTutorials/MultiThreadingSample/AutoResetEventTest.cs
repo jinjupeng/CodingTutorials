a78c2b0c0b77d94b05d10a60c8fbfa24b56f6fa5
猫先生 <im.jp@outlook.com>
2022-03-29 18:17:06 +0800

    add AutoResetEvent and ManualResetEvent

---
+++ CSharpTutorials/MultiThreadingSample/AutoResetEventTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThreadingSample
+{
+    /// <summary>
+    /// AutoResetEvent一次只能通知一个等待线程，通知后自动关闭
+    /// 举例：像一个收费站，一次允许一辆车通过，并且通过之后自动关闭
+    /// </summary>
+    public class AutoResetEventTest
+    {
+        //若要将初始状态设置为终止，则为 true；若要将初始状态设置为非终止，则为 false
+        static AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+            {
+                Console.WriteLine("客户1在排队等待付钱...");
+
+                //客户1调用AutoResetEvent上的WaitOne来等待付钱通知
+                autoResetEvent.WaitOne();
+                Console.WriteLine("通知来了，客户1付钱");
+            });
+            t1.IsBackground = true;
+            t1.Start();
+
+            Pay();//发送通知
+            Console.ReadKey();
+        }
+
+        static void Pay()
+        {
+            string tip = Console.ReadLine();
+            if (tip == "next")
+            {
+                autoResetEvent.Set();//收银员发送付钱通知，通过调用Set来通知客户付钱
+            }
+        }
+    }
+}
---
+++ CSharpTutorials/MultiThreadingSample/ManualResetEventTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadingSample
+{
+    /// <summary>
+    /// ManualResetEvent一次可通知很多个等待的线程，但要关闭需要调用Reset方法手动关闭。
+    /// 举例：像一个门，需要手动关闭
+    /// </summary>
+    class ManualResetEventTest
+    {
+        //若要将初始状态设置为终止，则为 true；若要将初始状态设置为非终止，则为 false。
+        static ManualResetEvent manualResetEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+            {
+                Console.WriteLine("客户1在排队等待付钱...");
+
+                //客户1调用manualResetEvent上的WaitOne来等待付钱通知
+                manualResetEvent.WaitOne();
+                Console.WriteLine("已经有人请客，客户1不用付钱");
+            });
+            t1.IsBackground = true;
+            t1.Start();
+
+            Thread t2 = new Thread(() =>
+            {
+                Console.WriteLine("客户2在排队等待付钱...");
+
+                //客户2调用manualResetEvent上的WaitOne来等待付钱通知
+                manualResetEvent.WaitOne();
+                Console.WriteLine("已经有人请客，客户2不用付钱！");
+            });
+            t2.IsBackground = true;
+            t2.Start();
+
+            Pay();//发送通知
+
+            Console.ReadKey();
+        }
+
+        static void Pay()
+        {
+            string tip = Console.ReadLine();
+            if (tip == "next")
+            {
+                manualResetEvent.Set();//收银员发送付钱通知，通过调用Set来通知客户付钱
+            }
+        }
+    }
+}
